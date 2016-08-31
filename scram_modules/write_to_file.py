d43870d363f78607aafc9fcd03af7a368834e2c1
Stephen Fletcher <s.fletcher@uq.edu.au>
2016-08-31 12:03:25 +1000

    Update write to file module

--- scram_modules/write_to_file.py
+++ scram_modules/write_to_file.py
@@ -4,7 +4,7 @@ Created on 22 Jan 2016
 @author: steve
 '''
 """
-Write results to file
+Write results to file (csv format)
 """
 
 import csv
@@ -23,20 +23,27 @@ def csv_output(alignment_dict, nt, seq_file_name, header):
     with open(header+'_'+seq_file_name+'_'+str(nt)\
               +'.csv', 'wb') as csvfile:
         mycsv = csv.writer(csvfile, delimiter=',')
-        mycsv.writerow(['sRNA','Position', 'Count'])
+        mycsv.writerow(['sRNA','5-prime nuc. position', 'Count'])
         for alignment in alignment_list:
-            out=[str(alignment[0]),alignment[1],alignment[2]]
+            out=[str(alignment[0]),alignment[1]+1,alignment[2]]
             mycsv.writerow(out)
     csvfile.close()
 
 
 def mnt_csv_output(alignment_dict_21, alignment_dict_22, alignment_dict_24,
                     seq_file_name, header):
+    """
+    For mnt - write 3 seperate csvs for 21,22,24 nt sRNA lengths
+    """
     csv_output(alignment_dict_21, 21, seq_file_name, header)
     csv_output(alignment_dict_22, 22, seq_file_name, header)
     csv_output(alignment_dict_24, 24, seq_file_name, header)        
 
 def cdp_output(counts_by_ref, header1, header2, out_file):
+    """
+    Write to file --> csv
+    sRNA, seq1_count, seq2_count
+    """
     results_list=[]
     for header, counts in counts_by_ref.iteritems():
         results_list.append((header, counts[0], counts[1]))
@@ -49,19 +56,6 @@ def cdp_output(counts_by_ref, header1, header2, out_file):
 
     csvfile.close()
 
-# def cdp_single_output(counts_by_ref, header1, nt):
-#     out_file = "{0}_{1}nt.csv".format(header1, nt)
-#     results_list=[]
-#     for header, counts in counts_by_ref.iteritems():
-#         results_list.append((header, counts))
-#     with open(out_file, 'wb') as csvfile:
-#         mycsv = csv.writer(csvfile, delimiter=',')
-#         mycsv.writerow(['',header1])
-#         for result in results_list:
-#             out=[result[0],result[1]]
-#             mycsv.writerow(out)
-# 
-#     csvfile.close()
 
 
 
